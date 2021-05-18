763e6c8f088fa163e06b208be9f66f3182fd4375
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-19 02:29:08 +0400

    execute module code

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
+require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
 
-db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
