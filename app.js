827860c76752d5a9ac2dc2adce8959205efdaa9a
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-22 02:02:31 +0400

    fix: sequelize sync

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-require('./db');
+const db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
-
-app.use(require('body-parser'));
+db.sync();
+app.use(require('body-parser').json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function() {
     console.log("App is listening on 4000");
 })
