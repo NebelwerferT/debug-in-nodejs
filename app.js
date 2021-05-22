ceb5478e1ab7723c00053ef01f842a60a3a8a12c
NebelwerferT <NebelwerferT9@gmail.com>
2021-05-23 03:03:28 +0400

    refactor: express.json() in app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
 const db = require('./db');
 const user = require('./controllers/usercontroller');
 const game = require('./controllers/gamecontroller')
 
+const app = express();
+
 db.sync();
-app.use(require('body-parser').json());
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
